123a10756c91e06e59378936922d39f0bb332c10
alfcanr <80713668+alfcanr@users.noreply.github.com>
2021-03-15 13:09:41 -0600

    Update data_weighting.js

--- inst/rkward/plugins/data/data_weighting.js
+++ inst/rkward/plugins/data/data_weighting.js
@@ -1,23 +1,26 @@
 // author: Alfredo Sánchez Alberca (asalber@ceu.es)
-var dataframe,
-    freq,
-    newDataframe;
 
-function setGlobalVars() {
-    dataframe = getString("dataframe");
-    freq = getString("freq.shortname");
-    newDataframe = getString("newDataframe");
+// globals
+var data, newdata, freq;
+
+function preprocess(){
+	// add requirements etc. here
+	echo("require(rkTeaching)\n");
 }
 
-function preprocess() {
-    setGlobalVars();
-    echo("library(rkTeaching)\n");
+function calculate () {
+	data = getString("dataframe");
+	newdata = getString("save");
+	freq = getString("freq.shortname");
+	echo ('.GlobalEnv$' + newdata + ' <- weightDataFrame(' + data + ', "' + freq + '")\n');	
+	// Copy also the labels of original data set
+	echo('for(i in 1:length(names(' + newdata + '))){\n');
+	echo('\t if (names(' + newdata + ')[i]!= "' + freq + '"){\n');
+    echo('\t attr(.GlobalEnv$' + newdata + '[[names(' + newdata + ')[i]]],".rk.meta") = attr(' + data + '[[names(' + newdata + ')[i]]],".rk.meta")\n');
+    echo('\t }\n');
+    echo('}\n');
 }
 
-function calculate() {
-    echo('.GlobalEnv$' + newDataframe + ' <- weightDataFrame(' + dataframe + ', "' + freq + '")\n');
-}
-
-function printout() {
-    var header = new Header(i18n("Data weighting")).addFromUI("dataframe").addFromUI("freq").addFromUI("newDataframe").print();
+function printout () {
+	echo ('rk.header ("Ponderaci&oacute;n de conjunto de datos", parameters=list("Conjunto de datos a ponderar" = "' + data + '", "Frecuencias de ponderaci&oacute;n" = "' + freq + '", "Nuevo conjunto de datos" = rk.get.description(' + newdata + ')))\n');
 }
