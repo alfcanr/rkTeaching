7255aeffb65e241f80482769a7efc1333437d322
AlfCano <50886849+AlfCano@users.noreply.github.com>
2025-09-01 12:00:47 -0600

    Update uniform_continuous_probabilities.js
    Fixing qplot argument.
    
    By making this change, you define the desired plot range without forcing a specific and limited x aesthetic onto the stat_function layer, thus resolving the error.

--- inst/rkward/plugins/distributions/uniform_continuous_probabilities.js
+++ inst/rkward/plugins/distributions/uniform_continuous_probabilities.js
@@ -50,7 +50,7 @@ function printout() {
 		echo('y <- dunif(x,' + min + ',' + max + ')\n');
 		echo('rk.graph.on()\n');
 		echo('try ({\n');
-		echo('p <- qplot(x=c(' + min + ',' + max + '), geom="blank") + geom_area(aes(x=c(x[1],x,x[2]), c(0,y,0)), fill=I("#FF9999"), alpha=0.5) + stat_function(fun=dunif, n=2, colour="#FF5555", args=list(min=' + min + ', max=' + max + ')) + xlab(expression(italic("X"))) + ylab(expression(paste(' + i18n("Density") + '," ",italic(f(x))))) + scale_x_continuous(breaks=c(' + min + ',' + q + '[1],' + max + '))');
+		echo('p <- qplot(xlim=c(' + min + ',' + max + '), geom="blank") + geom_area(aes(x=c(x[1],x,x[2]), c(0,y,0)), fill=I("#FF9999"), alpha=0.5) + stat_function(fun=dunif, n=2, colour="#FF5555", args=list(min=' + min + ', max=' + max + ')) + xlab(expression(italic("X"))) + ylab(expression(paste(' + i18n("Density") + '," ",italic(f(x))))) + scale_x_continuous(breaks=c(' + min + ',' + q + '[1],' + max + '))');
 		if (tail == "lower.tail=TRUE") {
 			echo(' + labs(title=paste("P(X<",' + q + '[1], ")=", round(result[1],4)))\n');
 		} else {
